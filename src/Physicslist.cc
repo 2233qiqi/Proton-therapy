689deb2797f2955c17210132e604b181779942e9
2233qiqi <2233qiqi>
2025-10-12 20:59:22 +0800

    10/12

--- src/Physicslist.cc
+++ src/Physicslist.cc
@@ -4,3 +4,24 @@
 #include "G4EmStandardPhysics_option4.hh" 
 #include "G4HadronPhysicsFTFP_BERT_HP.hh"
 
+#include "G4ParticleDefinition.hh"
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
+
+
+#include "G4ProductionCutsTable.hh"
+#include "G4LossTableManager.hh"
+
+PhysicsList :: PhysicsList() : G4VModularPhysicsList()
+{
+   SetVerboseLevel(1); 
+
+   RegisterPhysics(new G4EmStandardPhysics_option4());
+
+   
+   RegisterPhysics(new G4HadronPhysicsFTFP_BERT_HP());
+
+   RegisterPhysics(new G4DecayPhysics());
+
+};
