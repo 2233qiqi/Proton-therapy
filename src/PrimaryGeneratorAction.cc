c19f15ef5e7e9029a34d7c5c8b88ee23e9e6898c
2233qiqi <2233qiqi>
2025-10-12 21:32:08 +0800

    10/12

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,1 +1,22 @@
 #include "PrimaryGeneratorAction.hh"
+
+#include "G4GeneralParticleSource.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+
+PrimaryGenerator :: PrimaryGenerator() : G4VUserPrimaryGeneratorAction()
+{
+    fGPS->~G4GeneralParticleSource();
+} 
+
+void PrimaryGenerator::GeneratePrimaries(G4Event *event)
+{
+    fGPS->GeneratePrimaryVertex(event);
+}
+PrimaryGenerator::~PrimaryGenerator()
+{
+    delete fGPS;
+}
