26404fc127c4b42420a8b3fdf5c5c3b9bfd0501e
2233qiqi <2233qiqi>
2025-10-19 20:47:52 +0800

    10/19

---
+++ build/CMakeFiles/Proton-therapy.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/qiqi/geant4/Proton-therapy/Proton-therapy.cc" "CMakeFiles/Proton-therapy.dir/Proton-therapy.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/Proton-therapy.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/ActionInitialization.cc" "CMakeFiles/Proton-therapy.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/ActionInitialization.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/DetectorConstruction.cc" "CMakeFiles/Proton-therapy.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/DetectorConstruction.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/EventAction.cc" "CMakeFiles/Proton-therapy.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/EventAction.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/Physicslist.cc" "CMakeFiles/Proton-therapy.dir/src/Physicslist.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/Physicslist.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/PrimaryGeneratorAction.cc" "CMakeFiles/Proton-therapy.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/RunAction.cc" "CMakeFiles/Proton-therapy.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/RunAction.cc.o.d"
+  "/home/qiqi/geant4/Proton-therapy/src/SteppingAction.cc" "CMakeFiles/Proton-therapy.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/Proton-therapy.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Proton-therapy.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proton-therapy.dir/Proton-therapy.cc.o"
+  "CMakeFiles/Proton-therapy.dir/Proton-therapy.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/EventAction.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/Physicslist.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/Physicslist.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/RunAction.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/Proton-therapy.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/Proton-therapy.dir/src/SteppingAction.cc.o.d"
+  "Proton-therapy"
+  "Proton-therapy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proton-therapy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
