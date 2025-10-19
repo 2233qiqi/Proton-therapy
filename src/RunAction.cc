f94bf327c9efff662f758ea440fa71abde679a18
2233qiqi <2233qiqi>
2025-10-19 19:49:28 +0800

    10/19

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,3 +1,44 @@
 #include "RunAction.hh"
+#include "G4THitsMap.hh"
+#include "G4UnitsTable.hh"
+#include "DetectorConstruction.hh"
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+
+RunAction::RunAction(DetectorConstruction* dec, G4VAnalysisManager* man)
+: G4UserRunAction(),
+  fDetConstruction(dec)
+{
+
+}
+
+RunAction::~RunAction()
+{}
+
+G4Run* RunAction::GenerateRun()
+{
+    return G4UserRunAction::GenerateRun();
+};
+
+void RunAction::BeginOfRunAction(const G4Run*run)
+{
+   
+    if (IsMaster()) 
+    {
+        ReadAndWriteDose(run);
+    }
+}
+
+void RunAction::EndOfRunAction(const G4Run* run)
+{
+   
+    if (IsMaster()) 
+    {
+        ReadAndWriteDose(run);
+    }
+}
 
 
