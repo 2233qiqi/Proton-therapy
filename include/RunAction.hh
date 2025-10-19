bcef96b0717efe86b2502219cc57037c270747c6
2233qiqi <2233qiqi>
2025-10-19 19:42:17 +0800

    10/19

--- include/RunAction.hh
+++ include/RunAction.hh
@@ -3,6 +3,25 @@
 
 #include "G4UserRunAction.hh"
 #include "G4Run.hh"
+#include "G4VAnalysisManager.hh"
+#include "DetectorConstruction.hh"
+#include "string"
 
+
+class RunAction : public G4UserRunAction
+{
+public:
+
+RunAction(DetectorConstruction *dec,G4VAnalysisManager* man);
+virtual void BeginOfRunAction(const G4Run*);
+virtual void EndOfRunAction(const G4Run*);
+
+private:
+
+DetectorConstruction* fDetConstruction;
+void ReadAndWriteDose(const G4Run* run);
+
+
+};
 
 #endif
