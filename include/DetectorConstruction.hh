b617589839b8160c19e61d6af33ee8c36cf99b5e
2233qiqi <2233qiqi>
2025-10-20 17:47:02 +0800

    10/20

---
+++ include/DetectorConstruction.hh
@@ -0,0 +1,31 @@
+#ifndef DetectorConstruction_h
+#define DetectorConstruction_h
+
+#include "G4VUserDetectorConstruction.hh"
+
+class G4VPhysicalVolume;
+class G4LogicalVolume;
+
+namespace B1
+{
+
+
+
+class DetectorConstruction : public G4VUserDetectorConstruction
+{
+  public:
+    DetectorConstruction() = default;
+    ~DetectorConstruction() override = default;
+
+    G4VPhysicalVolume* Construct() override;
+
+    G4LogicalVolume* GetScoringVolume() const { return fScoringVolume; }
+
+  protected:
+    G4LogicalVolume* fScoringVolume = nullptr;
+};
+
+} 
+
+
+#endif
---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,9 @@
+#include "DetectorConstruction.hh"
+
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4LogicalVolume.hh"
+#include "G4NistManager.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Trd.hh"
